aad4473fdf809a2234faa8d833f4e82bbdec14cc
schmandr <andreas.schmid@bd.so.ch>
2017-10-13 14:10:58 +0200

    Make commands executable more than once

--- jenkins/install_jenkins.sh
+++ jenkins/install_jenkins.sh
@@ -7,5 +7,5 @@ apt-get update
 apt-get install -y jenkins
 apt-get install -y zip
 wget -q https://services.gradle.org/distributions/gradle-3.5.1-bin.zip
-mkdir /opt/gradle
-unzip -d /opt/gradle gradle-3.5.1-bin.zip
+mkdir -p /opt/gradle
+unzip -d /opt/gradle -u gradle-3.5.1-bin.zip
