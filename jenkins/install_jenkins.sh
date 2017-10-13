209e9c154c8b4628ecbf5b7b43ef82f977e41f54
schmandr <andreas.schmid@bd.so.ch>
2017-10-13 14:04:06 +0200

    Moved install commands to separate file

---
+++ jenkins/install_jenkins.sh
@@ -0,0 +1,11 @@
+locale-gen de_CH.utf8
+echo "Europe/Zurich" | tee /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-get update
+apt-get install -y jenkins
+apt-get install -y zip
+wget -q https://services.gradle.org/distributions/gradle-3.5.1-bin.zip
+mkdir /opt/gradle
+unzip -d /opt/gradle gradle-3.5.1-bin.zip
